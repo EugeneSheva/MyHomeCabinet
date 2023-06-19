b31091b3f0b6f7c45fbe8770269d3b877f48c78c
EugeneSheva <eugenesheva5259@gmail.com>
2023-06-19 10:35:09 +0300

    19.06,2023 8

--- src/main/java/com/example/myhome/home/configuration/WebConfig.java
+++ src/main/java/com/example/myhome/home/configuration/WebConfig.java
@@ -30,12 +30,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         System.out.println("CONFIG UPLOAD PATH " + uploadPath);
         System.out.println("CONFIG PREFIX " + prefix);
-//        registry
-//                .addResourceHandler("/img/**", "/images/**")
-//                .addResourceLocations(prefix + uploadPath + "/", prefix + uploadPath + "/img/");
         registry
                 .addResourceHandler("/img/**", "/images/**")
-                .addResourceLocations("http://kino.avada-media-dev1.od.ua:9002/myhome/WEB-INF/classes/static/images/", "http://kino.avada-media-dev1.od.ua:9002/myhome/WEB-INF/classes/static/images", "http://kino.avada-media-dev1.od.ua:9002/WEB-INF/classes/static/images/", "http://kino.avada-media-dev1.od.ua:9002/WEB-INF/classes/static/images");
+                .addResourceLocations(prefix + uploadPath + "/", prefix + uploadPath + "/img/");
+//        registry
+//                .addResourceHandler("/img/**", "/images/**")
+//                .addResourceLocations("http://kino.avada-media-dev1.od.ua:9002/myhome/WEB-INF/classes/static/images/", "http://kino.avada-media-dev1.od.ua:9002/myhome/WEB-INF/classes/static/images", "http://kino.avada-media-dev1.od.ua:9002/WEB-INF/classes/static/images/", "http://kino.avada-media-dev1.od.ua:9002/WEB-INF/classes/static/images");
         registry
                 .addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
