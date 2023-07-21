b73af6d305749eef328fb302532cf5b26b2df732
Inspiron <Eugenesheva5259@gmail/com>
2023-07-21 16:42:03 +0300

    21.07,2023 debug

--- src/test/java/com/example/myhome/MyHomeApplicationTests.java
+++ src/test/java/com/example/myhome/MyHomeApplicationTests.java
@@ -11,42 +11,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class MyHomeApplicationTests {
 
-    @MockBean
-    private AccountServiceImpl accountServiceImpl;
+
     @MockBean
     private AdminServiceImpl adminService;
     @MockBean
     private ApartmentServiceImpl apartmentService;
-    @MockBean
-    private BuildingService buildingService;
+
     @MockBean
     private InvoiceService invoiceService;
     @MockBean
     private MessageService messageService;
-    @MockBean
-    private MeterDataServiceImpl meterDataService;
+
     @MockBean
     private OwnerService ownerService;
     @MockBean
     private RepairRequestService repairRequestService;
-    @MockBean
-    private ServiceServiceImpl serviceService;
-    @MockBean
-    private TariffServiceImpl tariffService;
+
 
     @Test
     void contextLoads() {
-        assertThat(accountServiceImpl).isNotNull();
         assertThat(apartmentService).isNotNull();
         assertThat(adminService).isNotNull();
-        assertThat(buildingService).isNotNull();
         assertThat(invoiceService).isNotNull();
         assertThat(messageService).isNotNull();
-        assertThat(meterDataService).isNotNull();
         assertThat(ownerService).isNotNull();
         assertThat(repairRequestService).isNotNull();
-        assertThat(serviceService).isNotNull();
-        assertThat(tariffService).isNotNull();
+
     }
 
 }
