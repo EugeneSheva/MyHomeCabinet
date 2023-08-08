3979f27371cec7c145fa9ae83e6be79bd88ef9c4
Inspiron <Eugenesheva5259@gmail/com>
2023-08-08 21:56:14 +0300

    08/08/23 fogot pass,remember me, web socket del msg, modal windows..

--- src/main/java/com/example/myhome/home/configuration/AwsS3Config.java
+++ src/main/java/com/example/myhome/home/configuration/AwsS3Config.java
@@ -12,51 +12,51 @@ import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
-@Component
+//@Component
 public class AwsS3Config {
-    String secretName = "credentials";
-    String awsAccessKey;
-    String awsSecretKey;
-    Region region = Region.EU_NORTH_1;
-
-    @Value("${aws.bucket.name}")
-    private String awsBucket;
-    @Bean
-    public S3Client s3Client() {
-        getSecters();
-        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
-        return S3Client.builder()
-                .credentialsProvider(() -> awsCredentials)
-                .region(region)
-                .build();
-    }
-
-    private void getSecters() {
-        SecretsManagerClient secretsManagerClient = SecretsManagerClient.builder()
-                .region(region)
-                .build();
-
-        GetSecretValueRequest secretValueRequest = GetSecretValueRequest.builder()
-                .secretId(secretName)
-                .build();
-
-        try {
-            GetSecretValueResponse secretValueResponse = secretsManagerClient.getSecretValue(secretValueRequest);
-            String secretString = secretValueResponse.secretString();
-
-            System.out.println("Secret: " + secretString);
-
-            Gson gson = new Gson();
-            SecretData secretData = gson.fromJson(secretString, SecretData.class);
-
-            awsSecretKey = secretData.getSecretAccessKey();
-            awsAccessKey = secretData.getAccessKey();
-
-            System.out.println("SecretAccessKey: " + awsSecretKey);
-            System.out.println("AccessKey: " + awsAccessKey);
-
-        } catch (SecretsManagerException e) {
-            System.err.println("Error can`t get secrets: " + e.getMessage());
-        }
-    }
+//    String secretName = "credentials";
+//    String awsAccessKey;
+//    String awsSecretKey;
+//    Region region = Region.EU_NORTH_1;
+//
+//    @Value("${aws.bucket.name}")
+//    private String awsBucket;
+//    @Bean
+//    public S3Client s3Client() {
+//        getSecters();
+//        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(awsAccessKey, awsSecretKey);
+//        return S3Client.builder()
+//                .credentialsProvider(() -> awsCredentials)
+//                .region(region)
+//                .build();
+//    }
+//
+//    private void getSecters() {
+//        SecretsManagerClient secretsManagerClient = SecretsManagerClient.builder()
+//                .region(region)
+//                .build();
+//
+//        GetSecretValueRequest secretValueRequest = GetSecretValueRequest.builder()
+//                .secretId(secretName)
+//                .build();
+//
+//        try {
+//            GetSecretValueResponse secretValueResponse = secretsManagerClient.getSecretValue(secretValueRequest);
+//            String secretString = secretValueResponse.secretString();
+//
+//            System.out.println("Secret: " + secretString);
+//
+//            Gson gson = new Gson();
+//            SecretData secretData = gson.fromJson(secretString, SecretData.class);
+//
+//            awsSecretKey = secretData.getSecretAccessKey();
+//            awsAccessKey = secretData.getAccessKey();
+//
+//            System.out.println("SecretAccessKey: " + awsSecretKey);
+//            System.out.println("AccessKey: " + awsAccessKey);
+//
+//        } catch (SecretsManagerException e) {
+//            System.err.println("Error can`t get secrets: " + e.getMessage());
+//        }
+//    }
 }
